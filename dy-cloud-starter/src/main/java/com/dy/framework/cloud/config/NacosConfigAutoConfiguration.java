c9605dfe62172d85df65fb7dbce25d088d76b59e
戴源景 <dyj@ncweal.com>
2023-07-19 17:29:03 +0800

    build(dy-cloud-starter): 构建cloud模块，定义nacos配置中心自动刷新 及注册中心

---
+++ dy-cloud-starter/src/main/java/com/dy/framework/cloud/config/NacosConfigAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.dy.framework.cloud.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+/**
+ *启动配置自动刷新 自动配置类
+ * @author daiyuanjing
+ * @Date 2023-07-19
+ */
+@AutoConfiguration
+@RefreshScope
+public class NacosConfigAutoConfiguration {
+}
---
+++ dy-cloud-starter/src/main/java/com/dy/framework/cloud/config/NacosDiscoveryAutoConfiguration.java
@@ -0,0 +1,14 @@
+package com.dy.framework.cloud.config;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 启用服务发现 自动配置类
+ * @author daiyuanjing
+ * @date 2023-07-19
+ */
+@AutoConfiguration
+@EnableDiscoveryClient
+public class NacosDiscoveryAutoConfiguration {
+}
